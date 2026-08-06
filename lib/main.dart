b9698453c61892391d07e6bd03280b847f5b289d
Taraa90210 <zhafifwistara1@gmail.com>
2026-08-06 22:27:19 +0700

    12.2 sprint 3 HOTS 1

--- lib/main.dart
+++ lib/main.dart
@@ -110,42 +110,31 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       List<String> barangGudang = ['Krayon', 'Buku Tulis', 'Pensil', 'Rautan'];
       List<num> hargaBarang = [10000, 3000, 2500, 2000];
-      int stockBuku = 5;
+      List<int> stockBarang = [8, 10, 9, 5];
 
       final formatRupiah = NumberFormat.currency(
         locale: 'id_ID',
         symbol: 'Rp',
         decimalDigits: 0,
       );
-
+      num akumulator = 0;
       print('=====Penjualan Buku Tulis=====');
-      // for (int i = 0; i < barangGudang.length; i++ ) {
-      //   print('${i+1}. ${barangGudang[i]}-${formatRupiah.format(hargaBarang[i])}');
-      // }
-      while (stockBuku > 0) {
-        stockBuku = stockBuku - 2;
-        print('Terjual 1, Sisa stock: $stockBuku');
-      }
-      //Bahaya apa yang muncul bila kondisi berhenti pada while keliru, dan bagaimana 
-      //cara untuk memastikan koperasi tidak menjual melebihi stok?
-      //Jawaban:
-      //Jika kondisi while ditulis keliru, misalnya menggunakan 'stock != 0' alih-alih 'stock > 0',
-      //maka jika suatu saat nilai stok "terlewat" dari angka kecil langsung ke minus (misalnya karena
-      //kesalahan lain dalam program), kondisi 'stock != 0' akan tetap bernilai true (karena -1 != 0),
-      //sehingga perulangan tetap berjalan dan program bisa mencetak "sisa stok: -1" atau lebih kecil lagi.
-      //Ini berbahaya karena data yang ditampilkan menjadi tidak masuk akal secara bisnis - petugas koperasi
-      //bisa mengira barang masih "terjual" padahal stok fisiknya sudah habis, sehingga catatan penjualan
-      //jadi tidak akurat.
-      //
-      //Untuk memastikan koperasi tidak menjual melebihi stok, kondisi while sebaiknya menggunakan
-      //'stock > 0' (bukan '!= 0'). Dengan kondisi ini, begitu stok mencapai 0, perulangan otomatis
-      //berhenti karena '0 > 0' bernilai false. Untuk kasus penjualan satu per satu seperti pada soal ini
-      //(stok berkurang tepat 1 setiap kali), kondisi 'stock > 0' sudah cukup aman dan menjamin stok
-      //tidak akan pernah tercatat bernilai negatif.
+      for (int i = 0; i < barangGudang.length; i++) {
+        num nilaiBarang = hargaBarang[i] * stockBarang[i];
+        akumulator = akumulator + nilaiBarang;
+        print('${i+1}. ${barangGudang[i]} - Nilai Stock: ${formatRupiah.format(nilaiBarang)}');
+       }
+      print('Akumulasi: ${formatRupiah.format(akumulator)}');
       print('==============================');
     });
   }
 
+// for (int i = 0; i < barangGudang.length; i++ ) {
+      //   print('${i+1}. ${barangGudang[i]}-${formatRupiah.format(hargaBarang[i])}');
+      // }
+      // while (stockBuku > 0) {stockBuku = stockBuku - 2;
+      //   print('Terjual 1, Sisa stock: $stockBuku');
+      // }
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
