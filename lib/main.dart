6bcfb753442d271c55cfbe2d4fe6829c1bfea6f2
Taraa90210 <zhafifwistara1@gmail.com>
2026-08-06 13:23:10 +0700

    12.2 sprint 3 no. 1

--- lib/main.dart
+++ lib/main.dart
@@ -34,18 +34,82 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  // void _printStock() {
+  //   setState(() {
+  //     String barang = 'Pulpen 3 Warna';
+  //     num hargaMember = -25000;
+  //     num hargaUmum = 25000;
+  //     int jumlahStock = 30;
+  //     bool member = true;
+  //     num hargaSatuan;
+  //     num potongan;
+  //     List;
+
+  //     bool tersedia = jumlahStock > 0; //<----
+  //     num selisih = hargaUmum - hargaMember;
+
+  //     final formatRupiah = NumberFormat.currency(
+  //       locale: 'id_ID',
+  //       symbol: 'Rp',
+  //       decimalDigits: 0,
+  //     );
+
+  //     if (member == true) {
+  //       hargaSatuan = hargaMember;
+  //     } 
+  //     else {
+  //       hargaSatuan = hargaUmum;
+  //     }
+  //     num beliBerapa = 22;
+  //     num total = hargaSatuan * beliBerapa;
+
+  //     if (total > 500000 && member == true) {
+  //       potongan = total * (15/100);
+      // }
+      // else if (total > 200000) {
+      //   potongan = total * (10/100);
+      // }
+      // else if (total >   100000) {
+      //   potongan = total * (5/100);
+      // }
+      // else {
+      //   potongan = 0;
+      // }
+
+      // num hargaAkhir = total - potongan;
+
+  //   //jarak la
+  //   String kategori = 'atk';
+  //   String lokasiRak;
+  //   switch (kategori) {
+  //     case 'atk' : lokasiRak = 'Rak 1';
+  //     case 'makanan' : lokasiRak = 'Rak 2';
+  //     case 'minuman' : lokasiRak = 'Rak 3';
+  //     default : lokasiRak = 'Rak Lain';
+  //   };
+  //   //Kalau pakai banyak if-else if harus menulis ekspresi pengecekan variabel kategori
+  //   //berulang-ulang di setiap baris kondisi
+  //   //
+  //     print('=====Stock=====');
+  //     print('Barang: $barang');
+  //     print('Rak: $lokasiRak');
+  //     print('Harga Member: ${formatRupiah.format(hargaMember)}');
+  //     print('Harga Umum: ${formatRupiah.format(hargaUmum)}');
+  //     print('Jumlah Stock: $jumlahStock');
+  //     print('Tersedia: ${tersedia ? "Tersedia" : "Tidak Tersedia"}'); //<----
+  //     print('Status Pembeli: ${member ? "Member" : "Umum"}');
+  //     print('Total Barang yang di Beli: $beliBerapa pcs');
+  //     print('Total: ${formatRupiah.format(total)}');
+  //     print('Potongan: ${potongan}');
+  //     print('Total (Potongan): ${formatRupiah.format(hargaAkhir)}');
+  //     print('Selisih Anggota dan Umum: ${formatRupiah.format(selisih)}');
+  //     print('===============');
+  //   });
+  // }
   void _printStock() {
     setState(() {
-      String barang = 'Pulpen 3 Warna';
-      num hargaMember = 25000;
-      num hargaUmum = 25000;
-      int jumlahStock = 30;
-      bool member = true;
-      num hargaSatuan;
-      num potongan;
-
-      bool tersedia = jumlahStock > 0; //<----
-      num selisih = hargaUmum - hargaMember;
+      List<String> barangGudang = ['Krayon', 'Buku Tulis', 'Pensil', 'Rautan'];
+      List<num> hargaBarang = [10000, 3000, 2500, 2000];
 
       final formatRupiah = NumberFormat.currency(
         locale: 'id_ID',
@@ -53,57 +117,11 @@ class _MyHomePageState extends State<MyHomePage> {
         decimalDigits: 0,
       );
 
-      if (member == true) {
-        hargaSatuan = hargaMember;
-      } 
-      else {
-        hargaSatuan = hargaUmum;
+      print('=====Daftar Barang=====');
+      for (int i = 0; i < barangGudang.length; i++ ) {
+        print('${i+1}. ${barangGudang[i]}-${formatRupiah.format(hargaBarang[i])}');
       }
-      num beliBerapa = 22;
-      num total = hargaSatuan * beliBerapa;
-
-      if (total > 500000 && member == true) {
-        potongan = total * (15/100);
-      }
-      else if (total > 200000) {
-        potongan = total * (10/100);
-      }
-      else if (total >   100000) {
-        potongan = total * (5/100);
-      }
-      else {
-        potongan = 0;
-      }
-
-      num hargaAkhir = total - potongan;
-
-    //jarak la
-    String kategori = 'atk';
-    String lokasiRak;
-    switch (kategori) {
-      case 'atk' : lokasiRak = 'Rak 1';
-      case 'makanan' : lokasiRak = 'Rak 2';
-      case 'minuman' : lokasiRak = 'Rak 3';
-      default : lokasiRak = 'Rak Lain';
-    };
-    //Kalau pakai banyak if-else if harus menulis ekspresi pengecekan variabel kategori
-    //berulang-ulang di setiap baris kondisi
-    //
-      print('=====Stock=====');
-      print('Barang: $barang');
-      print('Rak: $lokasiRak');
-      print('Harga Member: ${formatRupiah.format(hargaMember)}');
-      print('Harga Umum: ${formatRupiah.format(hargaUmum)}');
-      print('Jumlah Stock: $jumlahStock');
-      print('Tersedia: ${tersedia ? "Tersedia" : "Tidak Tersedia"}'); //<----
-      print('Status Pembeli: ${member ? "Member" : "Umum"}');
-      print('Total Barang yang di Beli: $beliBerapa pcs');
-      print('Total: ${formatRupiah.format(total)}');
-      print('Potongan: ${potongan}');
-      print('Total (Potongan): ${formatRupiah.format(hargaAkhir)}');
-      print('Selisih Anggota dan Umum: ${formatRupiah.format(selisih)}');
-      print('===============');
-      
+      print('=======================');
     });
   }
 
@@ -151,7 +169,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _printStock, //<----
+        onPressed: _printStock,
         tooltip: 'Cek stock',
         child: const Text('Cek'),
       ),
